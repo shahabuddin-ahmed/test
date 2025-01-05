ed75c204eba61dcb469addbc9340c22f7cced680
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2025-01-06 00:02:09 +0600

    Added text analyzer view

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
+import cors from "cors";
 import config from "./config/config";
 import { newV1Router } from "./web/router/v1/index";
 import { newAnalyzerRepo } from "./repo/analyzer";
@@ -10,6 +11,10 @@ import { initializeDBConnection } from "./infra/mongo";
 import { globalErrorHandler } from "./web/middleware/global-error-handler";
 
 const app = express();
+
+app.use(cors({
+    origin: true,
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
