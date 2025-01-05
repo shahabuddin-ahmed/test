3932fca263ae7d6183eda87b652af3c208eb4cc7
Shahabuddin Ahmed <shahabuddin.cse.ru@gmail.com>
2025-01-05 18:31:22 +0600

    updated collection name

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
     );
 
     // Initialize Repo
-    const analyzerRepo = await newAnalyzerRepo(db, "analyzer");
+    const analyzerRepo = await newAnalyzerRepo(db, "text");
 
     // Initialize Service
     const analyzerService = await newAnalyzerService(analyzerRepo);
